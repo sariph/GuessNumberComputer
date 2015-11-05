8bfa43a8fd24380fafafec0b0de62d654061c8a7
Sariph Shrestha <sariph.shrestha@gmail.com>
2015-11-05 01:51:16 -0500

    Initial Version of Computer Number Guess Game
    The computer guesses the number that is in your mind. The basic
    algorithm being used is Binary Search for narrowing down the upper
    bound and lower bounds.

---
+++ src/main/java/GuessNumber/GuessNumberComputer/ComputerNumberGuess.java
@@ -0,0 +1,77 @@
+package GuessNumber.GuessNumberComputer;
+
+import java.util.Scanner;
+
+/**
+ * @author Sariph Shrestha
+ *
+ */
+
+public class ComputerNumberGuess {
+	public static final String READY="ready";
+	public static final String HIGHER="higher";
+	public static final String LOWER="lower";
+	public static final String END="end";
+	public static final String QUESTION="Is the Number ";
+	public static final String QUESTION_MARK="?";
+	
+	/**
+	 * The range of values was not specified in the problem
+	 * So if we assume the range to be maximum value of the Integer then there might be an overflow
+	 * There are few solutions
+	 * 1) Increase the size of the data type to higher[long, BigInteger]
+	 * 2) Decrease the value of the maximum and minimum to half so that it does not overflow
+	 * public static int lowerBound=Integer.MIN_VALUE/2;
+	 * public static int upperBound=Integer.MAX_VALUE/2;
+	 */
+	public static int lowerBound=-100;
+	public static int upperBound=100;
+	
+	private final Scanner scanner;
+	
+	public ComputerNumberGuess(){
+		this(new Scanner(System.in));
+	}
+	
+	/**
+	 * For Dependency Injection(If needed)
+	 * @param scanner
+	 */
+	public ComputerNumberGuess(Scanner scanner) {
+		this.scanner=scanner;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getNext(){
+		return scanner.next();
+	}
+	
+	public void guessNumber(){
+			//Take the user Input
+			String userInput=getNext();
+			
+			//The game begins
+			if(userInput.equalsIgnoreCase(READY)){
+				while(!userInput.equalsIgnoreCase(END)){
+					int currentGuess=(lowerBound + upperBound)/2;
+					System.out.println(QUESTION+currentGuess+QUESTION_MARK);
+					
+					//Get User Response
+					userInput=getNext();
+					if(userInput.equalsIgnoreCase(HIGHER)){
+						lowerBound=currentGuess+1;
+					} else if(userInput.equalsIgnoreCase(LOWER)){
+						upperBound=currentGuess-1;
+					}
+				}
+			}
+		return;
+	}
+
+	public static void main(String args[]){
+		ComputerNumberGuess computerNumberGuess=new ComputerNumberGuess(new Scanner(System.in));
+		computerNumberGuess.guessNumber();
+	}
+}
---
+++ src/test/java/GuessNumber/GuessNumberComputer/ComputerNumberGuessTest.java
@@ -0,0 +1,13 @@
+package GuessNumber.GuessNumberComputer;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ComputerNumberGuessTest {	
+	@Test
+	public void testMain() {
+		ComputerNumberGuess.main(new String[] {});
+		assertTrue(true);
+	}
+
+}
